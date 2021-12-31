778365ec5e38409f9d19f5eadd62df4df6af7ee8
leightek <leighsuconsulting@gmail.com>
2021-12-30 22:44:33 -0800

    [SpringBatch] Added hello world job

---
+++ hello-world/src/test/java/com/leightek/batch/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leightek.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
